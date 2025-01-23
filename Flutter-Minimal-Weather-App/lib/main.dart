0102619b64d5e4c7a7ab517752a1f1323c07974d
Muthunilavan-D <muthunilavand@gmail.com>
2025-01-23 21:02:30 +0530

    fix: Locked screenOrientation to Portrait

--- Flutter-Minimal-Weather-App/lib/main.dart
+++ Flutter-Minimal-Weather-App/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:weather_app/pages/weather_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
