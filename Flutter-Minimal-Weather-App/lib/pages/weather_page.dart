3a6e07ef25b62340887eabd9a3abb38b0381360b
Muthunilavan-D <muthunilavand@gmail.com>
2025-01-23 20:47:12 +0530

    fix: Fixed weather condition issue

--- Flutter-Minimal-Weather-App/lib/pages/weather_page.dart
+++ Flutter-Minimal-Weather-App/lib/pages/weather_page.dart
@@ -26,44 +26,155 @@ class _WeatherPageState extends State<WeatherPage> {
       final weather = await _weatherService.getWeather(cityName);
       setState(() {
         _weather = weather;
+        condition = getWeatherCondition(_weather?.mainCondition); // Update the condition
       });
     } catch (e) {
       print("Error fetching weather: $e");
     }
   }
 
-  String getWeatherAnimation(String? maincondition) {
-    if (maincondition == null) return 'assets/sunny.json';
+  String getWeatherCondition(String? mainCondition) {
+    if (mainCondition == null) return "CONDITION: Sunny";
 
-     switch (maincondition.toLowerCase()) {
+    switch (mainCondition.toLowerCase()) {
       case 'clouds':
-        {
-          condition = "CONDITION: Cloudy";
-          return 'assets/cloud.json';
-        }
-
+        return "CONDITION: Cloudy";
       case 'mist':
-        {
-          condition = "CONDITION: Mist";
-          return 'assets/mist.json';
-        }
+        return "CONDITION: Mist";
       case 'haze':
-        {
-          condition = "CONDITION: Haze";
-          return 'assets/haze.json';
-        }
+        return "CONDITION: Haze";
       case 'fog':
-        {
-          condition = "CONDITION: Foggy";
-          return 'assets/cloud.json';
-        }
+        return "CONDITION: Foggy";
       case 'dust':
-        {
-          condition = "CONDITION: Dusty";
-          return 'assets/cloud.json';
-        }
+        return "CONDITION: Dusty";
       case 'smoke':
-        {
+        return "CONDITION: Smoky";
+      case 'rain':
+        return "CONDITION: Raining";
+      case 'drizzle':
+        return "CONDITION: Drizzling";
+      case 'shower rain':
+        return "CONDITION: Shower Rain";
+      case 'thunderstorm':
+        return "CONDITION: Thunderstorm";
+      case 'clear':
+        return "CONDITION: Clear";
+      default:
+        return "Sunny";
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+        return 'assets/cloud.json';
+      case 'mist':
+        return 'assets/mist.json';
+      case 'haze':
+        return 'assets/haze.json';
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'dust':
+        return 'assets/cloud.json';
+      case 'smoke':
+        return 'assets/cloud.json';
+      case 'rain':
+        return 'assets/rain.json';
+      case 'drizzle':
+        return 'assets/rain.json';
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return "assets/thunder.json";
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 206, 203, 203),
+        toolbarHeight: 30,
+        title: Text(
+          "Minimal Weather App!",
+          style: GoogleFonts.slabo27px(
+              color: const Color.fromARGB(255, 23, 30, 34),
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(229, 27, 27, 27),
+          Color.fromARGB(228, 53, 48, 48)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              const Icon(
+                Icons.location_on,
+                color: Color.fromARGB(255, 217, 218, 218),
+              ),
+              Text(
+                _weather?.cityName ?? "Loading..",
+                style: GoogleFonts.bebasNeue(
+                    color: const Color.fromARGB(255, 13, 91, 134),
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                condition,
+                style: GoogleFonts.belanosima(
+                  fontSize: 36,
+                  color: const Color.fromARGB(255, 189, 212, 224),
+                ),
+              ),
+              const Spacer(),
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+              const Spacer(),
+              Container(
+                alignment: Alignment.center,
+                width: 120,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: const LinearGradient(colors: [
+                      Color.fromARGB(255, 244, 125, 145),
+                      Color.fromARGB(255, 249, 180, 140)
+                    ])),
+                child: Text('${_weather?.temperature.round()} °C',
+                    style: GoogleFonts.macondo(
+                        color: const Color.fromARGB(255, 7, 56, 82),
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold)),
+              ),
+              const SizedBox(
+                height: 25,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
           condition = "CONDITION: Smoky";
           return 'assets/cloud.json';
         }
